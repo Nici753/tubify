1025f0ffd7fbdd36442450975de2cae7c48b8ff0
Kohlei <alex.beer753@outlook.de>
2024-07-13 19:22:01 +0200

    adding theming

---
+++ src/components/layout/navigation.tsx
@@ -0,0 +1,11 @@
+import {ModeToggle} from "./mode-togle.tsx";
+
+export function Navigation() {
+
+    return (
+        <div>
+            <ModeToggle/>
+        </div>
+    )
+}
+export default Navigation
