f6e3acf99c914b176c9c2c04860bd200166258bf
kohlei <alex.beer753@outlook.de>
2024-07-16 20:11:50 +0200

    displaying the playlists

--- src/components/playlists/sidebar.tsx
+++ src/components/playlists/sidebar.tsx
@@ -5,15 +5,39 @@ import React from 'react';
 export const Sidebar: React.FC = () => {
   const playlists = useSelector((state: PlaylistState) => {
     console.log(state);
-    return state.playlists;
+    return state.playlists.playlists;
   });
 
   if (playlists.length === 0) {
-    return <p>No playlists available.</p>;
+    return (
+      <div className={'flex flex-col p-3 border-r-2 h-full break-words'}>
+        <p>
+          Import some Playlists from Spotify first :D
+        </p>
+      </div>
+    );
   }
 
   return (
     <div className={'flex flex-col p-3 border-r-2 h-full break-words'}>
+      <h2 className={'text-2xl font-bold'}>Your Playlists</h2>
+      <div className="grid grid-cols-1 gap-2">
+        {playlists.map((playlist) => (
+          <div
+            key={playlist.SpotifyId}
+            className="flex items-center space-x-2 p-1 cursor-pointer"
+          >
+            <img
+              src={playlist.imageUrl}
+              alt={playlist.name}
+              className="w-6 h-6 rounded"
+            />
+            <span className="flex-grow text-left truncate">
+              {playlist.name}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
