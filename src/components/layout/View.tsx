1ece7394fe5fc27cc61ce313b2bccbf402ab3774
Kohlei <alex.beer753@outlook.de>
2024-07-16 12:58:15 +0200

    adding toaster for future toasts

--- src/components/layout/View.tsx
+++ src/components/layout/View.tsx
@@ -1,11 +1,13 @@
 import TopBar from './top-bar.tsx';
 import { PageMainContent } from './page-main-content.tsx';
+import { Toaster } from '../ui/toaster.tsx';
 
 export function View() {
   return (
     <div className={'flex flex-col h-screen'}>
       <TopBar />
       <PageMainContent />
+      <Toaster />
     </div>
   );
 }
