a1b5204e3fb9724bcc0e020ac493f9dd1204fe99
kohlei <alex.beer753@outlook.de>
2024-07-17 19:19:28 +0200

    music player

---
+++ src/components/playlists/SpotifyMusicPlayer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface SpotifyMusicPlayerProps {
+  spotifyUri: string;
+  parentWidth: number;
+}
+
+const SpotifyMusicPlayer: React.FC<SpotifyMusicPlayerProps> = ({
+  spotifyUri,
+  parentWidth,
+}) => {
+  const src = `https://open.spotify.com/embed/track/${spotifyUri}`;
+
+  return (
+    <iframe src={src} width={parentWidth} height="100" allow="encrypted-media"></iframe>
+  );
+};
+
+export default SpotifyMusicPlayer;
--- src/components/playlists/content.tsx
+++ src/components/playlists/content.tsx
@@ -1,18 +1,38 @@
 import { useSelector } from 'react-redux';
 import { PlaylistState } from '../../lib/types/Playlist.ts';
 import { CirclePlay } from 'lucide-react';
+import SpotifyMusicPlayer from './SpotifyMusicPlayer.tsx';
+import { useEffect, useState } from 'react';
 
 export function Content() {
   const selectedPlaylist = useSelector(
     (state: PlaylistState) => state.playlists.selectedPlaylist,
   );
 
+  const [currentSong, setCurrentSong] = useState<string | null>(null);
+  const [parentWidth, setParentWidth] = useState<number>(null);
+
+  useEffect(() => {
+    const updateParentWidth = () => {
+      if (!document.getElementById('parent-width')) {
+        return null;
+      } else {
+        setParentWidth(document.getElementById('parent-width').clientWidth - 24);
+      }
+    };
+    updateParentWidth();
+    window.addEventListener('resize', updateParentWidth);
+    return () => {
+      window.removeEventListener('resize', updateParentWidth);
+    };
+  }, []);
+
   if (!selectedPlaylist) {
     return null;
   }
 
   return (
-    <div className={'col-span-4 lg:col-span-3 flex flex-col p-3 h-fulls'}>
+    <div className={'col-span-4 lg:col-span-3 flex flex-col px-3 pt-3 pb-32 h-fulls'} id="parent-width">
       <h1 className={'text-3xl font-bold'}>{selectedPlaylist.name}</h1>
       <div className="grid grid-cols-1 gap-2">
         {selectedPlaylist.tracks?.map((song) => (
@@ -22,7 +42,7 @@ export function Content() {
           >
             <CirclePlay
               className="w-8 h-8 opacity-0 group-hover:opacity-100 hover:cursor-pointer"
-              //onClick={() => onSongClick(song.SpotifyId)}
+              onClick={() => setCurrentSong(song.SpotifyId)}
             />
             <img
               src={song.imageUrl}
@@ -37,6 +57,9 @@ export function Content() {
             </div>
           </div>
         ))}
+      </div>
+      <div className={'fixed bottom-2'}>
+        {currentSong && <SpotifyMusicPlayer spotifyUri={currentSong} parentWidth={parentWidth}/>}
       </div>
     </div>
   );
