dd172c62791746096edb54b1f69525e4fbca493d
kohlei <alex.beer753@outlook.de>
2024-07-27 18:28:55 +0200

    searching a song on YouTube

--- src/lib/apis/YoutubeAPIService.ts
+++ src/lib/apis/YoutubeAPIService.ts
@@ -1,4 +1,5 @@
 import { Playlist } from '../types/Playlist.ts';
+import { Song } from '../types/Song.ts';
 
 export interface YoutubeAPIInterface {}
 
@@ -14,10 +15,49 @@ export class YoutubeAPIService implements YoutubeAPIInterface {
     return YoutubeAPIService.instance;
   }
 
-  async searchSongOnYoutube(querry: string): Promise<Response> {}
+  async youtubeRequest(endpoint: string, method: string): Promise<Response> {
+    const request: Request = new Request(
+      `https://www.googleapis.com/youtube/v3${endpoint}`,
+      {
+        method: method,
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('youtube_access_token')}`,
+        },
+      },
+    );
+    return await fetch(request);
+  }
+
+  async searchSongOnYoutube(song: Song): Promise<Song> {
+    const query: string = `${song.name} ${song.artists.join(', ')}`;
+    console.log('Query: ' + query);
+
+    const search_url: string = `?part=snippet&maxResults=1&q=${query}`;
+    const response: Response = await this.youtubeRequest(
+      `/search/${search_url}`,
+      'GET',
+    );
+    const data: JSON = await response.json();
+    console.log('Data: ' + data);
+    if (data.items.length > 0) {
+      song.YoutubeId = data.items[0].id.videoId;
+      song.YoutubeUrl = `https://www.youtube.com/watch?v=${song.YoutubeId}`;
+    }
+    return song;
+  }
 
   async updatePlaylist(playlist: Playlist): Promise<Playlist> {
     console.log(playlist);
+    playlist.tracks?.forEach((song) => {
+      const youtubeSong = this.searchSongOnYoutube(song);
+      if (youtubeSong.YoutubeId) {
+        song.YoutubeId = youtubeSong.YoutubeId;
+        song.YoutubeUrl = youtubeSong.YoutubeUrl;
+      }
+      console.log(song);
+    });
+    console.log('New Playlist: ' + playlist);
+
     return playlist;
   }
 }
