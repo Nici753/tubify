906c5092e7840b1e88952905b993b4de455790fc
Kohlei <alex.beer753@outlook.de>
2025-03-02 17:20:12 +0100

    delete login data

--- src/components/login/logout-button.tsx
+++ src/components/login/logout-button.tsx
@@ -2,19 +2,16 @@ import { Button } from '../ui/Button.tsx';
 import { LogOut } from 'lucide-react';
 
 export function LogoutButton() {
-  function handleLogin() {
+
+  function handleLogout() {
     // localStorage.clear();
-    localStorage.removeItem('spotify_access_token');
-    localStorage.removeItem('youtube_access_token');
-    localStorage.removeItem('user_name');
-    localStorage.removeItem('user_email');
-    localStorage.removeItem('user_picture');
+    localStorage.removeItem('user-data');
     window.location.reload();
   }
 
   return (
     <div className={'mr-3'}>
-      <Button variant="outline" onClick={handleLogin}>
+      <Button variant="outline" onClick={handleLogout}>
         <LogOut className={'h-[1.2rem] w-[1.2rem] mr-1'} />
         Logout
       </Button>
