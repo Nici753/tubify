7b3d933a945f04db1022a7e6d71633eda1870944
Kohlei <alex.beer753@outlook.de>
2026-05-18 20:22:15 +0200

    removed some obsolete code, restyled loading toast, added new ideas

--- src/components/layout/View.tsx
+++ src/components/layout/View.tsx
@@ -1,13 +1,16 @@
 import TopBar from './top-bar.tsx';
 import { PageMainContent } from './page-main-content.tsx';
 import { Toaster } from 'sonner';
+import { useTheme } from './theme-provider';
 
 export function View() {
+  const { theme } = useTheme()
+
   return (
     <div className={'flex flex-col h-screen'}>
       <TopBar />
       <PageMainContent />
-      <Toaster richColors={true} theme={'system'} />
+      <Toaster richColors={true} theme={theme as 'light' | 'dark' | 'system'}/>
     </div>
   );
 }
